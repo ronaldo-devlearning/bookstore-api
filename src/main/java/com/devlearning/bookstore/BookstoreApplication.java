91b78eb2268ab9b954abdcb6c6a11920abeab230
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-11-05 21:24:29 -0300

    Criado o perfil de teste do H2

--- src/main/java/com/devlearning/bookstore/BookstoreApplication.java
+++ src/main/java/com/devlearning/bookstore/BookstoreApplication.java
@@ -1,41 +1,13 @@
 package com.devlearning.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devlearning.bookstore.domain.Categoria;
-import com.devlearning.bookstore.domain.Livro;
-import com.devlearning.bookstore.repositories.CategoriaRepository;
-import com.devlearning.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	@Autowired
-	private LivroRepository livroRepository;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem Ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-		
-	}
-
 }
---
+++ src/main/java/com/devlearning/bookstore/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.devlearning.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.devlearning.bookstore.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaBaseDeDados() {
+		dbService.instanciaBaseDeDados();
+	}
+}
